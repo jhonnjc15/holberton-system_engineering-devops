bc91f135770815d83ba7db9330ea2493f0e2a08b
jhonnjc15 <jhonatanjcca@gmail.com>
2021-08-14 01:54:44 +0000

    Fxing some erros

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Create a manifest that kills a process named killmenow
 
-exec { 'kill':
-	comand => 'pkill -f killmenow',
-	path => '/usr/bin/:/usr/local/bin/:/bin/'
+exec { 'killmenow':
+	command => 'pkill -f killmenow',
+	path => '/usr/bin/:/usr/local/bin/:/bin/',
 }
