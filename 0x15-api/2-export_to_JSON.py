4120d10c6161c78fe0958f5fa6e38804d5659426
jhonnjc15 <jhonatanjcca@gmail.com>
2021-09-13 21:58:10 +0000

    Script to export data in the JSON format.

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Script that, using this REST API https://jsonplaceholder.typicode.com/,
+for a given employee ID, returns information
+about his/her TODO list progress"""
+import requests
+import sys
+import json
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    name = user.get("username")
+
+    data = []
+    for i in todos:
+        data.append({
+                "task": i.get("title"),
+                "completed": i.get("completed"),
+                "username": name})
+
+    with open("{}.json".format(user_id), "w", encoding="utf-8") as json_file:
+        json.dump({user_id: [i for i in data]}, json_file)
