534f9c559a1d507abfbf21a60f505169c0bcf0a7
jhonnjc15 <jhonatanjcca@gmail.com>
2021-09-28 21:53:42 -0500

    removing the json library

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Funciton that returns the number of subscribers"""
-    url = "https://api.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://api.reddit.com/r/{}/about".format(subreddit)
     head = {"User-Agent": "Client"}
     request = requests.get(url, headers=head)
     if request.status_code != 200:
