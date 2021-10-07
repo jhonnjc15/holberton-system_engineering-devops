476a05c162477191de42bdcfd8b58b0a89b02442
jhonnjc15 <jhonatanjcca@gmail.com>
2021-10-06 22:56:31 -0500

    Web stack debugging #4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+# Fxing huge amount of failed requests
+exec { 'fix--for-nginx':
+  command => 'sed -i s/15/500/g /etc/default/nginx',
+  path    => ['/usr/bin', '/usr/sbin', '/bin/'],
+}
+->
+exec { 'restart-nginx':
+  command => 'sudo service nginx restart',
+  path    => ['/usr/bin', '/usr/sbin',],
+}
